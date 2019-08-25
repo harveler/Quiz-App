8efe107f903466536a943a0e1d6c095ffa5c2a53
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-08-25 14:22:36 +0930

    Commit backbone files

---
+++ Capitals/Data/QuizDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Capitals.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capitals.Data
+{
+    public class QuizContext : DbContext
+    {
+        public DbSet<Quiz> Quizzes { get; set; }
+
+        public DbSet<WordlCapital> WorldCapitals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=quizapp;Username=postgres;Password=password");
+    }
+}
---
+++ Capitals/Models/WorldCapital.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capitals.Models
+{
+    public class WordlCapital
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<Country> Countries { get; set; }
+        public List<Capital> Capitals { get; set; }
+        public int Difficulty { get; set; }
+    }
+}
