b241a0308f2f9a75d5e45d20db847ab238fdafb2
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-09-06 20:06:07 +0800

    Recreated home component with CLI

--- Capitals/CapitalsApp/src/app/app.module.ts
+++ Capitals/CapitalsApp/src/app/app.module.ts
@@ -5,17 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 // components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '**', component: AppComponent, pathMatch: 'full' },
+      { path: '**', component: HomeComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
