0e0dc09b98d49b9d7559d0fc52daab06eb70e733
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-09-12 16:58:40 +0800

    Create question models in front and back end

---
+++ Capitals/CapitalsApp/src/app/Models/questionmodel.ts
@@ -0,0 +1,7 @@
+export class IQuestion {
+    countryName: string;
+    option1: string;
+    option2: string;
+    option3: string;
+    option4: string;
+}
---
+++ Capitals/Models/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,13 @@
+namespace Capitals.Models.ViewModels
+{
+    public class QuestionViewModel
+    {
+        public string CountryName { get; set; }
+        public string CapitalName { get; set; }
+
+        public string FirstOption { get; set; }
+
+        public string SecondOption { get; set; }
+        public string ThirdOption { get; set; }
+    }
+}
