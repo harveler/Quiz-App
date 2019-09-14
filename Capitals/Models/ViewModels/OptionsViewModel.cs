b8a3a193c7c238e7cabb05070c89c25a56ebe1d8
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-09-14 11:12:29 +0800

    Changed viewmodel structure for Questions and Options

---
+++ Capitals/Models/ViewModels/OptionsViewModel.cs
@@ -0,0 +1,12 @@
+namespace Capitals.Models.ViewModels
+{
+    public class Options
+    {
+        public string CapitalName { get; set; }
+
+        public string FirstOption { get; set; }
+
+        public string SecondOption { get; set; }
+        public string ThirdOption { get; set; }
+    }
+}
--- Capitals/Models/ViewModels/QuestionViewModel.cs
+++ Capitals/Models/ViewModels/QuestionViewModel.cs
@@ -1,13 +1,10 @@
+using Capitals.Models.ViewModels;
+
 namespace Capitals.Models.ViewModels
 {
     public class QuestionViewModel
     {
         public string CountryName { get; set; }
-        public string CapitalName { get; set; }
-
-        public string FirstOption { get; set; }
-
-        public string SecondOption { get; set; }
-        public string ThirdOption { get; set; }
+        public Options Options { get; set; }
     }
 }
