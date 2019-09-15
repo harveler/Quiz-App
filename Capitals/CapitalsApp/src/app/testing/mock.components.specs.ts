cca4d011e96655760d48e540c3d9822086eeb535
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-09-15 21:16:53 +0800

    Create file for mock components

---
+++ Capitals/CapitalsApp/src/app/testing/mock.components.specs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-mock-score',
+    template: '<div>Dummy home component</div>',
+    styleUrls: []
+})
+
+export class DummyScoreCardComponent { }
+
+
